1f7e879030deb925522f51f46cfde6ce385a5866
weley ortega <wesslleyortegga12@gmail.com>
2024-08-29 12:51:18 -0400

    marina lanche

---
+++ js/dados.js
@@ -0,0 +1,455 @@
+var MENU = {
+    "churrasco": [
+        {
+            "id": "ribs-brisket-and-burnt-ends",
+            "img": "./img/cardapio/churrasco/joes-kc-ribs-brisket-and-burnt-ends.6710e994980e485e6441b794717ad6fb.jpg",
+            "name": "Joe's KC BBQ",
+            "dsc": "Joe's KC Ribs, Brisket & Burnt Ends",
+            "price": 110.99
+        },
+        {
+            "id": "005-kings-carolina-oink-sampler",
+            "img": "./img/cardapio/churrasco/carolina-bbq-oink-sampler.1340b5a10cedc238cb2280306dd1d5a5.jpg",
+            "name": "Kings BBQ",
+            "dsc": "Carolina BBQ Oink Sampler",
+            "price": 89
+        },
+        {
+            "id": "texas-monthlys-1-bbq-brisket",
+            "img": "./img/cardapio/churrasco/texas-monthlys-1-bbq-brisket.1006a061be7acae03992e420fbca995a.jpg",
+            "name": "Snow's BBQ",
+            "dsc": "Texas Monthly's #1 BBQ Brisket",
+            "price": 199
+        },
+        {
+            "id": "whole-brisket-texas-bbq-sauce",
+            "img": "./img/cardapio/churrasco/whole-brisket-texas-barbecue-bbq-sauce.e07ee4818b20ed43d217bf67fedd41ce.jpg",
+            "name": "Franklin Barbecue",
+            "dsc": "Whole Brisket + Texas Barbecue BBQ Sauce",
+            "price": 249
+        },
+        {
+            "id": "whole-texas-smoked-brisket",
+            "img": "./img/cardapio/churrasco/whole-texas-smoked-brisket.a5558a25381e271408e197936e7985d8.jpg",
+            "name": "Terry Black's Barbecue",
+            "dsc": "Whole Texas Smoked Brisket",
+            "price": 189
+        },
+        {
+            "id": "mini-trinity-bbq-combo-brisket-ribs-and-links",
+            "img": "./img/cardapio/churrasco/mini-trinity-bbq-combo-brisket-ribs-and-links.245582f593bf64b23b57dfca2be18cfd.jpg",
+            "name": "Bludso's BBQ",
+            "dsc": "Mini Trinity BBQ Combo - Brisket, Ribs & Links",
+            "price": 139
+        },
+        {
+            "id": "235203-blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo",
+            "img": "./img/cardapio/churrasco/blue-smoke-baby-back-ribs-backyard-barbecue-chicken-combo.a95a3e632ae324f719738a2a5c1dff6e.jpg",
+            "name": "Blue Smoke",
+            "dsc": "Blue Smoke Baby Back Ribs + Backyard Barbecue Chicken Combo",
+            "price": 129
+        },
+        {
+            "id": "006-kings-meat-lovers-special",
+            "img": "./img/cardapio/churrasco/bbq-meat-lovers-special-for-10.36ca670fda4bfa783c2ea9165e068d26.jpg",
+            "name": "Kings BBQ",
+            "dsc": "BBQ Meat Lovers Special for 10",
+            "price": 139
+        },
+        {
+            "id": "the-big-ugly-bbq-dinner-for-6",
+            "img": "./img/cardapio/churrasco/the-big-ugly-bbq-dinner-for-6.2dfae7818811adddce85cc1a910881a0.jpg",
+            "name": "Ugly Drum",
+            "dsc": "The Big Ugly BBQ Dinner for 6",
+            "price": 229
+        },
+        {
+            "id": "17796-mighty-quinns-bbq-sampler-pack",
+            "img": "./img/cardapio/churrasco/mighty-quinns-bbq-sampler-pack.1bfe4a0665edc565756f5241bf25840e.jpg",
+            "name": "Mighty Quinn's BBQ",
+            "dsc": "Mighty Quinn's BBQ Sampler Pack",
+            "price": 169
+        },
+        {
+            "id": "post-oak-smoked-half-brisket",
+            "img": "./img/cardapio/churrasco/post-oak-smoked-usda-prime-half-brisket.6f17178dde7d806670bcc73ff11762b3.jpg",
+            "name": "Southside Market & Barbeque",
+            "dsc": "Post Oak Smoked USDA Prime Half Brisket",
+            "price": 109
+        },
+        {
+            "id": "best-of-texas-bbq-combo-serves-14",
+            "img": "./img/cardapio/churrasco/best-of-texas-bbq-combo-serves-14.7ae90266335e539c67e77fed14b43029.jpg",
+            "name": "Snow's BBQ",
+            "dsc": "Best of Texas BBQ Combo - Serves 14",
+            "price": 269
+        }
+    ],
+    "burgers": [
+        {
+            "id": "x-salada-card",
+            "img": "./img/cardapio/burguers/x-salada-card.jpg",
+            "name": "X-Salada",
+            "dsc": "pão de hamburguer, maionese, hamburguer, cheddar, presunto, alface e tomate",
+            "price": 13
+        },
+        {
+            "id": "x-burguer-marina",
+            "img": "./img/cardapio/burguers/x-burguer-marina.png",
+            "name": "X-BURGUER",
+            "dsc": "Pão de hambúrguer, maionese, hambúrguer, presunto.",
+            "price": 12
+        },
+        {
+            "id": "pão-de-hamburguer",
+            "img": "./img/cardapio/burguers/pão-de-hamburguer.png",
+            "name": "X-AMERICANO",
+            "dsc": "Pão de hambúrguer, ovo, maionese, Presunto, alface e tomate.",
+            "price": 10
+        },
+        {
+            "id": "x-bacon-bacon",
+            "img": "./img/cardapio/burguers/x-bacon-bacon.jpeg",
+            "name": "X-BACON",
+            "dsc": "Pão de hambúrguer, maionese, hambúrguer, bacon, cheddar, Presunto, alface e tomate.",
+            "price": 18
+        },
+        {
+            "id": "x-burguer-marina",
+            "img": "./img/cardapio/burguers/x-burguer-marina.jpg",
+            "name": "X-BURGUER ESPECIAL",
+            "dsc": "Pão de hambúrguer, maionese, hambúrguer, ovo, bacon, presunto, alface e tomate.",
+            "price": 22
+        },
+        {
+            "id": "X-EGG-MARINA",
+            "img": "./img/cardapio/burguers/X-EGG-MARINA.png",
+            "name": "X-EGG",
+            "dsc": "Pão de hambúrguer, maionese, hambúrguer, ovo, cheddar, presunto, alface e tomate.",
+            "price": 16
+        },
+        {
+            "id": "x-bacon-calabresa",
+            "img": "./img/cardapio/burguers/x-bacon-e-calabresa.jpg",
+            "name": "X-BACON E CALABRESA",
+            "dsc": "Pão de hambúrguer, maionese, hambúrguer, bacon, calabresa, cheddar, presunto, calabresa, alface e tomate.",
+            "price": 22
+        },
+        {
+            "id": "x-moda-da-casa",
+            "img": "./img/cardapio/burguers/the-burger-bash-package.bd9d12d031865940bbe5faf15f1a62f8.jpg",
+            "name": "X-MODA DA CASA",
+            "dsc": "Pão de hambúrguer, maionese, 2 hambúrguer, bacon, calabresa, 2 ovo, salsicha, cheddar, presunto, alface e tomate.",
+            "price": 35
+        },
+        {
+            "id": "x-tudo",
+            "img": "./img/cardapio/burguers/xtudo-79047.jpg",
+            "name": "X-TUDO",
+            "dsc": "Pão de hambúrguer, maionese, batata palha, milho verde, presunto, hambúrguer, bacon, calabresa, ovo, salsicha, cheddar, alface e tomate.",
+            "price": 28
+        },
+        {
+            "id": "x-baguncinha",
+            "img": "./img/cardapio/burguers/X-BAGUNCINHA.png",
+            "name": "X-BAGUNCINHA",
+            "dsc": "Pão de hambúrguer, maionese, batata palha, milho verde, bacon, calabresa, ovo, salsicha, cheddar, presunto, mussarela, alface e tomate.",
+            "price": 25
+        },
+        {
+            "id": "x-especial",
+            "img": "./img/cardapio/burguers/burger-au-poivre-kit-4-pack.3ca0e39b02db753304cd185638dad518.jpg",
+            "name": "X-ESPECIAL",
+            "dsc": "Pão de hambúrguer, hambúrguer, maionese, presunto, ovo, salsicha, cheddar, alface e tomate",
+            "price": 22
+        },
+        {
+            "id": "X-CALABRESA",
+            "img": "./img/cardapio/burguers/Gramercy-Tavern-Burger-and-Kielbasa-Kit-6.4.21-72ppi-1x1-47.jpg",
+            "name": "X-CALABRESA",
+            "dsc": "Pão de hambúrguer, maionese, hambúrguer, calabresa, cheddar, presunto, alface e tomate.",
+            "price": 18
+        }
+    ],
+    "sobremesas": [
+        {
+            "id": "15259-german-chocolate-killer-brownie-tin-pack",
+            "img": "./img/cardapio/sobremesas/german-chocolate-killer-brownie-tin-pack.5ebc34160f28767a9d94c4da2e04c4b9.jpg",
+            "name": "Killer Brownie®",
+            "dsc": "German Chocolate Killer Brownie®",
+            "price": 39.99
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies",
+            "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-6-pack.2217a14c443602493bba88aa9335319a.jpg",
+            "name": "Jacques Torres Chocolate",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 6 Pack",
+            "price": 39.95
+        },
+        {
+            "id": "luigis-original-cannoli-pie",
+            "img": "./img/cardapio/sobremesas/original-cannoli-pie.4cb5b9ba82f57b69b90765fd9f07aa1a.jpg",
+            "name": "The Cannoli Pie Company",
+            "dsc": "Original Cannoli Pie",
+            "price": 69
+        },
+        {
+            "id": "sea-salted-caramel-swirl-cheesecake",
+            "img": "./img/cardapio/sobremesas/sea-salted-caramel-swirl-cheesecake.e2825335433fb7a272a5d77649a6849e.jpg",
+            "name": "Cotton Blues Cheesecake Company",
+            "dsc": "Sea-Salted Caramel Swirl Cheesecake",
+            "price": 65
+        },
+        {
+            "id": "brooklyn-blackout-cookie-brownie-combo-pack-2-tins",
+            "img": "./img/cardapio/sobremesas/brooklyn-blackout-cookie-brownie-combo-pack-2-tins.d8805325baf6b23b4f01d119dc4531a7.jpg",
+            "name": "Brooklyn Blackout Company",
+            "dsc": "Brooklyn Blackout Cookie + Brownie Combo Pack - 2 Tins",
+            "price": 89
+        },
+        {
+            "id": "best-seller-cupcake-dozen",
+            "img": "./img/cardapio/sobremesas/best-seller-cupcake-dozen.f93b21993f6a2da11c975d45b9b0d08f.jpg",
+            "name": "Crave Cupcakes",
+            "dsc": "Best Seller Cupcake Dozen",
+            "price": 89
+        },
+        {
+            "id": "choose-your-own-ice-cream-donuts-6-pack",
+            "img": "./img/cardapio/sobremesas/choose-your-own-ice-cream-donuts-6-pack.24d0b44765a7c54237fcd7ea9d9d8093.jpg",
+            "name": "Elegant Desserts",
+            "dsc": "Choose Your Own Ice Cream Donuts - 6 Pack",
+            "price": 69
+        },
+        {
+            "id": "17481-jewish-dessert-3-pack",
+            "img": "./img/cardapio/sobremesas/jewish-classics-dessert-pack.7d4b76630f2fe75dcb7bbcf2501b7390.jpg",
+            "name": "Ess-a-Bagel",
+            "dsc": "Jewish Classics Dessert Pack",
+            "price": 89.95
+        },
+        {
+            "id": "dessert-bar-care-package",
+            "img": "./img/cardapio/sobremesas/dessert-bar-care-package.324aa28fe98c9dc67b75aac43376902e.jpg",
+            "name": "Bread and Roses Bakery",
+            "dsc": "Dessert Bar Care Package",
+            "price": 65
+        },
+        {
+            "id": "donut-cookies-12-pack",
+            "img": "./img/cardapio/sobremesas/donut-cookies-12-pack.46f42c61c4a79fd2051a99b2f23e890e.jpg",
+            "name": "Stan's Donuts",
+            "dsc": "Donut Cookies - 12 Pack",
+            "price": 49
+        },
+        {
+            "id": "gulab-jamun-ice-cream-cakes-2-pack",
+            "img": "./img/cardapio/sobremesas/gulab-jamun-ice-cream-cakes-2-pack.c45b4d0750ad22d741f84dc1f26d20e7.jpg",
+            "name": "Malai Ice Cream",
+            "dsc": "Gulab Jamun Ice Cream Cakes - 2 Pack",
+            "price": 79
+        },
+        {
+            "id": "jacques-world-famous-chocolate-chip-cookies-12-pack",
+            "img": "./img/cardapio/sobremesas/jacques-world-famous-chocolate-chip-cookies-12-pack.3b373bdd67cd25084182c21499f675d1.jpg",
+            "name": "Jacques Torres Chocolate",
+            "dsc": "Jacques' World Famous Chocolate Chip Cookies - 12 Pack",
+            "price": 69.95
+        }
+    ],
+    "bebidas": [
+        {
+            "id": "COCA-COLA-2-LITROS",
+            "img": "./img/cardapio/bebidas/coca-cola-2l.jpg",
+            "name": "Coca-Cola 2L",
+            "dsc": "coca-coal 2litros",
+            "price": 12
+        },
+        {
+            "id": "COCA-600-ML",
+            "img": "./img/cardapio/bebidas/COCA-600ML.jpg",
+            "name": "Coca-Cola 600ML",
+            "dsc": "coca 600",
+            "price": 6
+        },
+        {
+            "id": "coca-lata-350",
+            "img": "./img/cardapio/bebidas/Coca-Cola_Lata_750x600px.webp",
+            "name": "Coca-Cola lata ",
+            "dsc": "coca lata 350ml",
+            "price": 5
+        },
+        {
+            "id": "GUARANA-ANTARTICA-1L",
+            "img": "./img/cardapio/bebidas/antartica-1l.png",
+            "name": "Guarana Antartica 1L",
+            "dsc": "1L",
+            "price": 6
+        },
+        {
+            "id": "FUNDA-2L",
+            "img": "./img/cardapio/bebidas/FUNADA.jpeg",
+            "name": "Funada 2L",
+            "dsc": "2L",
+            "price": 10
+        },
+        {
+            "id": "COCA1L",
+            "img": "./img/cardapio/bebidas/COCACOCACOAL.jpg",
+            "name": "Coca-Cola 1L",
+            "dsc": "1L",
+            "price": 7
+        },
+        {
+            "id": "FANTA-2L",
+            "img": "./img/cardapio/bebidas/FANTA-2L.webp",
+            "name": "FANTA 2L",
+            "dsc": "2L",
+            "price": 12
+        }
+    ],
+    "pizzas": [
+        {
+            "id": "HOTDOG-ESPECIAL-PRENSADO",
+            "img": "./img/cardapio/pizzas/prensadohotdog.jpg",
+            "name": "Hot Dog prensado especial",
+            "dsc": "Pão de hambúrguer, maionese, catchup, batata palha, milho verde, bacon, calabresa, salsicha.",
+            "price": 15
+        },
+        {
+            "id": "HOTDOG-DUPLO-PRENSADO",
+            "img": "./img/cardapio/pizzas/hotduplo.jpg",
+            "name": "Hot Dog duplo prensado",
+            "dsc": "Pão de hot dog, maionese, catchup, Catupiry, batata palha, milho verde, 2 salsicha.",
+            "price": 10
+        },
+        {
+            "id": "HOTDOG-EGG-PRENSADO",
+            "img": "./img/cardapio/pizzas/hotdogegg.jpg",
+            "name": "Hot egg prensado",
+            "dsc": "Pão de Hambúrguer, ovo, alface, tomate, batata palha, salsicha, maionese, catchup.",
+            "price": 12
+        },
+        {
+            "id": "HOT-DOG-BACON",
+            "img": "./img/cardapio/pizzas/hot-bacon-marina.jpg",
+            "name": "Hot bacon prensado",
+            "dsc": "Pão de Hambúrguer, bacon, alface tomate, salsicha, maionese, Ketchup",
+            "price": 14
+        },
+        {
+            "id": "HOT-DOG-CALABRESA-PRENSADO",
+            "img": "./img/cardapio/pizzas/hot-calabresa-claa.jpg",
+            "name": "Hot calabresa prensado",
+            "dsc": "Pão de Hambúrguer, calabresa, Alface, Tomate, salsicha, maionese, Ketchup",
+            "price": 14
+        },
+        {
+            "id": "HOT-DOG-FRANGO-PRENSADO",
+            "img": "./img/cardapio/pizzas/dogfrango.jpg",
+            "name": "Hot frango especial prensado",
+            "dsc": "Pão de Hambúrguer, frango, salsicha, batata palha, Ketchup, maionese, milho verde.",
+            "price": 14
+        },
+        {
+            "id": "HOT-DOG-JUNTO-E-MISTURADO",
+            "img": "./img/cardapio/pizzas/juntoemisturado.jpg",
+            "name": "Hot junto e misturado",
+            "dsc": "Pão de hambúrguer, maionese, catchup, batata palha, milho verde, bacon, calabresa, frango, salsicha.",
+            "price": 18
+        },
+        {
+            "id": "HOT-DOG-ESPECIAL-5",
+            "img": "./img/cardapio/pizzas/5dogs.jpeg",
+            "name": "5 Hot Dogs especiais",
+            "dsc": "Pão de hambúrguer, maionese, catchup, batata palha, milho verde, bacon, calabresa, salsicha.",
+            "price": 75
+        }
+    ],
+    "steaks": [
+        {
+            "id": "california-reserve-filet-mignon-steaks-gift-box",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steaks-gift-box.bf226e317aad85f47897ae7e325f790d.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steaks Gift Box",
+            "price": 129
+        },
+        {
+            "id": "steaks-and-cakes-date-night-dinner-for-2",
+            "img": "./img/cardapio/steaks/valentines-steaks-and-cakes-dinner-for-2.0c63dab635eed46209455dc33cd25ea8.jpg",
+            "name": "Chesapeake Bay Gourmet",
+            "dsc": "Steaks and Cakes Date Night Dinner for 2",
+            "price": 129
+        },
+        {
+            "id": "Prime-holiday-steak-sampler-for-10-12",
+            "img": "./img/cardapio/steaks/holiday-sampler-9-pack.2de75ca80282ffe9d064eb757ff7a5a1.jpg",
+            "name": "Saltbrick Prime",
+            "dsc": "Chef Matt's Steak Sampler for 10-12",
+            "price": 179
+        },
+        {
+            "id": "bone-in-rib-steak",
+            "img": "./img/cardapio/steaks/bone-in-rib-steak.b13d9d4233035767605f0de9acdce1ab.jpg",
+            "name": "Old Homestead Steakhouse",
+            "dsc": "Bone-in Rib Steak",
+            "price": 159
+        },
+        {
+            "id": "american-wagyu-gold-grade-top-sirloins",
+            "img": "./img/cardapio/steaks/american-wagyu-gold-grade-sirloins.040dbeb8f8e615b91fa7e513e3dc089c.jpg",
+            "name": "Snake River Farms",
+            "dsc": "American Wagyu Gold Grade Top Sirloins",
+            "price": 119
+        },
+        {
+            "id": "2-peter-luger-steak-pack-b",
+            "img": "./img/cardapio/steaks/peter-luger-steak-pack-b.9feb0300e6be2dfecfa314f2006a2183.jpg",
+            "name": "Peter Luger Steak House",
+            "dsc": "Peter Luger Porterhouse Steaks",
+            "price": 215.95
+        },
+        {
+            "id": "ribeye-prime-steak-gift-box",
+            "img": "./img/cardapio/steaks/ribeye-prime-steak-gift-box.e74cb016baabbb2df73861de8150f29c.jpg",
+            "name": "Churchill's Steakhouse",
+            "dsc": "Ribeye Prime Steak Gift Box",
+            "price": 229
+        },
+        {
+            "id": "dry-aged-usda-prime-black-angus-porterhouse-steak-2-pack",
+            "img": "./img/cardapio/steaks/usda-prime-black-angus-filet-mignon-barrel-cut.6ee213799e7d1848763d12edca18e3b1.jpg",
+            "name": "Pat LaFrieda Meats",
+            "dsc": "Dry-Aged USDA Prime Black Angus Porterhouse Steak - 2 Pack",
+            "price": 96.7
+        },
+        {
+            "id": "california-reserve-ribeye-steak",
+            "img": "./img/cardapio/steaks/california-reserve-ribeye-steak-12-oz.d367c5ae72dd9f89e170662104bef4fc.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Ribeye Steak - 12 oz",
+            "price": 32
+        },
+        {
+            "id": "dry-aged-boneless-ribeye-steak-dinner-kit-for-4",
+            "img": "./img/cardapio/steaks/dry-aged-boneless-ribeye-steak-dinner-for-4.81c3bdc05fe6bdb2c2214709863120e0.jpg",
+            "name": "Chef Francis Mallmann",
+            "dsc": "Dry-Aged Boneless Ribeye Steak Dinner Kit for 4",
+            "price": 225
+        },
+        {
+            "id": "california-reserve-filet-mignon-steak",
+            "img": "./img/cardapio/steaks/california-reserve-filet-mignon-steak.ff15071964ec8141d30c2ba05fb117e0.jpg",
+            "name": "Flannery Beef",
+            "dsc": "California Reserve Filet Mignon Steak",
+            "price": 22
+        },
+        {
+            "id": "mesquite-smoked-peppered-beef-tenderloin",
+            "img": "./img/cardapio/steaks/mesquite-smoked-peppered-beef-tenderloin.5c314418a1f75c7057eed686e2fad46f.jpg",
+            "name": "Perini Ranch Steakhouse",
+            "dsc": "Mesquite Smoked Peppered Beef Tenderloin",
+            "price": 165
+        }
+    ]
+}
